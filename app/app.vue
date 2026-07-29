eb612167c9172872cfcf257e44ab00bd4a306c03
noppharat0th <uiavgcgccggv@gmail.com>
2026-07-29 22:35:07 +0700

    setup

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+onMounted(() => {
+
+  fetch('/api/or')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+})
+</script>
