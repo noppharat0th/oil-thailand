1ab25aaa2d91e7f8507532fe1705f04ff8a6a137
noppharat0th <uiavgcgccggv@gmail.com>
2026-07-30 11:33:55 +0700

    update show data

--- app/composables/prices.ts
+++ app/composables/prices.ts
@@ -1,17 +1,19 @@
 export const fetch_price = async (brand: string = 'ptt') => {
     try {
         const brands = ['ptt', 'bcp', 'shell', 'caltex', 'pt', 'susco']
+        let result = []
 
         for (const brand of brands) {
             try {
                 const res = await fetch(`/api/prices/${brand}`)
                 const data = await res.json()
+                result.push(data)
                 // console.log(`=== ${brand.toUpperCase()} ===`, data)
-                return data
             } catch (e) {
                 console.error(e)
             }
         }
+        return result
     } catch (e) {
         console.error(e)
     }
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,14 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
-    
+    <div v-for="brand in oil_data">
+        <h1>{{ brand.brand }}</h1>
+        <p>{{ brand.data }}</p>
+    </div>
 </template>
+
+<script setup>
+const oil_data = ref()
+
+onMounted(async () => {
+    oil_data.value = await fetch_price();
+})
+</script>
